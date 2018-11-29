ce787a6558d9b303c0f7e01abbba8cd58a3b9198
yhz286840222 <286840222@qq.com>
2018-11-29 18:01:08 +0800

    netty helloworld

--- src/main/java/com/yhz/moudle/helloworld/Client.java
+++ src/main/java/com/yhz/moudle/helloworld/Client.java
@@ -1,9 +1,55 @@
 package com.yhz.moudle.helloworld;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
 /**
  * @Auther: yanghz
  * @Date: 2018/11/29 14:12
  * @Description:客户端
  */
 public class Client {
+    private final String host;
+    private final int port;
+
+    public Client(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() throws InterruptedException {
+        EventLoopGroup group=new NioEventLoopGroup();
+        try {
+            //创建Bootstrap
+            Bootstrap b=new Bootstrap();
+            b.group(group)
+                    //指定 EventLoopGroup 以处理客户端事件；需要适用于 NIO 的实现适用于 NIO 传输的Channel 类型
+                    .channel(NioSocketChannel.class)
+                    //设置服务器的InetSocketAddress
+                    .remoteAddress(new InetSocketAddress(host,port))
+                    //在创建Channel时，向 ChannelPipeline中添加一个 ClientHandler 实例
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new ClientHandler());
+                        }
+                    });
+            //连接到远程节点， 阻塞等待直到连接完成
+            ChannelFuture f=b.connect().sync();
+            //阻塞， 直到Channel 关闭
+            f.channel().closeFuture().sync();
+        }finally {
+            //关闭线程池并且释放所有的资源
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new Client("127.0.0.1",2048).start();
+    }
 }
--- src/main/java/com/yhz/moudle/helloworld/Server.java
+++ src/main/java/com/yhz/moudle/helloworld/Server.java
@@ -1,9 +1,62 @@
 package com.yhz.moudle.helloworld;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
 /**
  * @Auther: yanghz
  * @Date: 2018/11/29 14:12
  * @Description:服务器端
  */
 public class Server {
+
+    private final int port;
+    public Server(int port){
+        this.port=port;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        if(args.length!=1){
+            System.out.println("Usage:"+Server.class.getSimpleName()+"<port>");
+        }
+        //设置端口值（如果端口参数的格式不正确，则抛出一个NumberFormatException）
+        //调用服务器的start方法
+        new Server(2048).start();
+
+    }
+
+    public void start() throws InterruptedException {
+        final ServerHandler serverHandler=new ServerHandler();
+        //创建EventLoopGroup
+        EventLoopGroup group=new NioEventLoopGroup();
+        try{
+            //创建ServerBootstrap
+            ServerBootstrap b=new ServerBootstrap();
+            b.group(group)
+                    .channel(NioServerSocketChannel.class)//指定所使用的NIO传输Channel
+                    .localAddress(new InetSocketAddress(port))//使用指定的端口设置套接字地址
+                    //添加一个ServerHandler到子Channel的ChannelPipeline
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            //EchoServerHandler 被标注为@Shareable，所以我们可以总是使用同样的实例
+                            socketChannel.pipeline().addLast(serverHandler);
+                        }
+                    });
+            //异步地绑定服务器；调用 sync()方法阻塞等待直到绑定完成
+            ChannelFuture f=b.bind().sync();
+            //获取 Channel 的CloseFuture，并且阻塞当前线程直到它完成
+            f.channel().closeFuture().sync();
+        }finally {
+            //关闭 EventLoopGroup， 程直到它完成释放所有的资源
+            group.shutdownGracefully().sync();
+        }
+    }
 }
