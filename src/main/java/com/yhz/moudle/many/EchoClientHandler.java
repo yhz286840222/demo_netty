36c70a31bffb115fffc56c711484d9889dc2a2d3
yhz286840222 <286840222@qq.com>
2018-11-30 17:59:09 +0800

    netty 拆包粘包

---
+++ src/main/java/com/yhz/moudle/many/EchoClientHandler.java
@@ -0,0 +1,38 @@
+package com.yhz.moudle.many;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @Auther: yanghz
+ * @Date: 2018/11/29 14:13
+ * @Description:客户端事件处理器
+ */
+//标记该类的实例可以被多个 Channel 共享
+@ChannelHandler.Sharable
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+    //接受服务器传输回来的数据
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+        System.out.println("EchoClient received: "+byteBuf.toString(CharsetUtil.UTF_8));
+    }
+
+    //连接成功通知，发送数据
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        //当被通知 Channel是活跃的时候，发送一条消息
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!",CharsetUtil.UTF_8));
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty jacks!",CharsetUtil.UTF_8));
+    }
+
+    //发生异常处理
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/main/java/com/yhz/moudle/many/EchoServerHandler.java
@@ -0,0 +1,44 @@
+package com.yhz.moudle.many;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @Auther: yanghz
+ * @Date: 2018/11/29 14:13
+ * @Description:服务器端事件处理器
+ */
+//标识一个ChannelHandler可以被多个Channel共享
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    //数据处理
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        //输出接受到客户端的消息
+        String requestData=in.toString(CharsetUtil.UTF_8);
+        System.out.println("EchoServer received:"+requestData);
+        String responseData="EchoServer response:"+requestData;
+        //将服务器响应消息写给发送者，而不冲刷出站消息
+        ctx.write(Unpooled.copiedBuffer(responseData.getBytes()));
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        //将未决消息冲刷到远程节点，并且关闭该Channel
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        //打印异常栈跟踪
+        cause.printStackTrace();
+        //关闭Channel
+        ctx.close();
+    }
+}
